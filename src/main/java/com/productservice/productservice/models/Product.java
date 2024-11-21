7ef7bbb33c8cf9abc8ef04b1642e87a9f4ec7abf
Shishir-vardhan <shishirvardhan695@gmail.com>
2024-11-21 16:29:34 +0530

    Database connectivity and table creation in db

--- src/main/java/com/productservice/productservice/models/Product.java
+++ src/main/java/com/productservice/productservice/models/Product.java
@@ -1,15 +1,32 @@
 package com.productservice.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
+@Entity // This tells Hibernate to make a table out of this class
+public class Product extends BaseModel {
 
     private String title;
     private String description;
     private int price;
     private String imageUrl;
+
+
+    //Category isn't a primitive attribute, it's a relation here.
+    @ManyToOne
     private Category category;
 }
+
+
+
+/*
+        1           1
+    product  ->  category
+        m           1
+       relation is - m:1
+ */
